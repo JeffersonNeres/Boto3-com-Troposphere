9681c48b196b53a77d72840ccc11bcf8fa59d0d5
Jefferson Neres <jeffersonjrima@gmail.com>
2021-10-07 16:03:59 -0300

    adicionando arquivos

---
+++ gerando_json.py
@@ -0,0 +1,33 @@
+from troposphere import Ref, Template, ec2
+from pprint import pprint
+
+template = Template()
+
+instancia =ec2.Instance(
+    "MentoramaEscolaOnline",
+    ImageId="ami-00534ec845573897f2",
+    InstanceType="t2.micro",
+)
+template.add_resource(instancia)
+
+template2 = Template()
+
+instancia =ec2.Instance(
+    "MonitoramentoDeServicos",
+    ImageId="ami-00534ec828473897g9",
+    InstanceType="t2.micro"
+)
+template2.add_resource(instancia)
+
+template3 = Template()
+
+instancia =ec2.Instance(
+    "cursoPythonPRO",
+    ImageId="ami-00534ec828834697h0",
+    InstanceType="t2.micro"
+)
+template3.add_resource(instancia)
+
+pprint(template.to_json())
+pprint(template2.to_json())
+pprint(template3.to_json())
